76cc0e28947d272d53d8ae03fa781293165dd2dc
WebDevCampos <webdevcampos@gmail.com>
2021-08-25 13:32:45 -0300

    Changes

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const addToDo = document.querySelector('.form-add-todo')
+const ul = document.querySelector('.todos-container')
 
+const sendToDo = (event) => {
+	event.preventDefault()
+
+	const task = event.target.add.value.trim()
+
+	// Se o imput não estiver vazio, envia o valor do input
+	if (task.length) {
+		ul.innerHTML += ` 
+	<li class="list-group-item d-flex justify-content-between align-items-center">
+		<span> ${task} </span>  
+		<i class="far fa-trash-alt delete">	</i>
+	</li>`
+	}
+	event.target.reset()
+} //Envio de afazeres / Send to-do list
+
+addToDo.addEventListener('submit', sendToDo)
