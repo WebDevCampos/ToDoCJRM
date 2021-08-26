127a9dbe51b1b88643c1e8aba5f19e053f2f449d
WebDevCampos <webdevcampos@gmail.com>
2021-08-26 14:18:19 -0300

    Changes

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const addToDo = document.querySelector('.form-add-todo')
-const ul = document.querySelector('.todos-container')
+const todoForm = document.querySelector('.form-add-todo')
 
-const sendToDo = (event) => {
+const taskContainer = document.querySelector('.todos-container')
+
+const sendTask = (event) => {
 	event.preventDefault()
+	const inputTask = event.target.add.value.trim()
 
-	const task = event.target.add.value.trim()
-
-	// Se o imput não estiver vazio, envia o valor do input
-	if (task.length) {
-		ul.innerHTML += ` 
-	<li class="list-group-item d-flex justify-content-between align-items-center">
-		<span> ${task} </span>  
-		<i class="far fa-trash-alt delete">	</i>
-	</li>`
+	if (inputTask.length) {
+		taskContainer.innerHTML += ` 
+		<li class="list-group-item d-flex justify-content-between align-items-center">
+			<span>${inputTask}</span>
+			<i class="far fa-trash-alt delete"></i>
+		</li>`
 	}
-	event.target.reset()
-} //Envio de afazeres / Send to-do list
+	todoForm.reset()
+}
 
-addToDo.addEventListener('submit', sendToDo)
+const removeTask = (event) => {
+	const clickedElement = event.target
+
+	if (Array.from(clickedElement.classList).includes('delete')) {
+		clickedElement.parentElement.remove()
+	}
+}
+
+todoForm.addEventListener('submit', sendTask)
+
+taskContainer.addEventListener('click', removeTask)
